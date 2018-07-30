139262ae8ebfa6d867b9f7fa347ea5915078f767
Tim Urista <timothy.urista@gmail.com>
2018-07-29 20:52:03 -0700

    add styling for robofriends bg

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './app';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
+import './app.css'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
