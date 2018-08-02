69a843ba9135cfab1d79b64882c9e968a6b348a6
Tim Urista <timothy.urista@gmail.com>
2018-08-01 21:10:38 -0700

    remove error from cardlist

--- robofriends/src/components/card-list.js
+++ robofriends/src/components/card-list.js
@@ -2,9 +2,6 @@ import React from 'react'
 import Card from '../components/card';
 
 const CardList = ({ robots }) => {
-  if (true) {
-    throw new Error('something went wrong')
-  }
   return (
     <div>
       {robots.map(({id, name, username, email}) => (
