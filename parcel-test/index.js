b8ffc9ce67786400d48e7e393ff08c6b5dabc260
Tim Urista <timothy.urista@gmail.com>
2018-08-05 14:19:44 -0700

    [MASTER-MASTER] add basic parcel setup using react

---
+++ parcel-test/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Hello = () => {
+  return (
+    <div>WHATSSSS UPPP</div>
+  )
+}
+
+ReactDOM.render(<Hello />, document.getElementById('app'))
