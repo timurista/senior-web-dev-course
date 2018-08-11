cd431d3dd1e2af9d7212bf735d4f5d13567ff149
Tim Urista <timothy.urista@gmail.com>
2018-08-10 22:07:48 -0700

    add simple jest testing example for node script

---
+++ test/script.test.js
@@ -0,0 +1,21 @@
+const googleSearch = require('./script');
+
+const dbMock = [
+  'dog.com',
+  'cheesepuff.com',
+  'disney.com',
+  'dogpictures.com',
+];
+
+// dependency injection, inject DB here by altering googleSearch function
+
+it('returns empty array if result is not found', () => {
+  expect(googleSearch('hello', dbMock)).toEqual([])
+})
+
+it('returns array of dogs when dog is passed in', () => {
+  expect(googleSearch('dog', dbMock)).toEqual([
+    'dog.com',
+    'dogpictures.com'
+  ])
+})
