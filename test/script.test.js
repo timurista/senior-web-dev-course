e9ac762dccfd5fc00e4ef9f120eb83c23174c1f5
Tim Urista <timothy.urista@gmail.com>
2018-08-11 07:45:41 -0700

    add tests to cover google search cases

--- test/script.test.js
+++ test/script.test.js
@@ -9,13 +9,24 @@ const dbMock = [
 
 // dependency injection, inject DB here by altering googleSearch function
 
-it('returns empty array if result is not found', () => {
-  expect(googleSearch('hello', dbMock)).toEqual([])
-})
-
-it('returns array of dogs when dog is passed in', () => {
-  expect(googleSearch('dog', dbMock)).toEqual([
-    'dog.com',
-    'dogpictures.com'
-  ])
+describe('googleSearch', () => {
+  it('returns empty array if result is not found', () => {
+    expect(googleSearch('hello', dbMock)).toEqual([])
+  })
+  
+  it('returns array of dogs when dog is passed in', () => {
+    expect(googleSearch('dog', dbMock)).toEqual([
+      'dog.com',
+      'dogpictures.com'
+    ])
+  })
+  
+  it('work with undefined or null', () => {
+    expect(googleSearch(undefined, dbMock)).toEqual([])
+    expect(googleSearch(null, dbMock)).toEqual([])
+  })
+  
+  it('does not return more than 3 matches', () => {
+    expect(googleSearch('.com', dbMock)).toHaveLength(3)
+  })
 })
