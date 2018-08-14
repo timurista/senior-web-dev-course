88ae8bc1d3d7c0c118dce547e13f2ea80ef68af9
Tim Urista <timothy.urista@gmail.com>
2018-08-14 06:48:10 -0700

    add more types on typescript types

--- typing/typescript.js
+++ typing/typescript.js
@@ -27,3 +27,42 @@ var Size;
 var sizeName = Size[2];
 // enums let us to define set of named constants
 // directions, or value associated with it
+// Any, be careful... use it right when using it
+var whatever = 'agghhh no!!!';
+whatever = 5;
+whatever = true;
+whatever = Size.small;
+// useful for when converting js objects to typescript
+// and this will take a long time
+// never
+var error = function () {
+    throw Error('oops');
+};
+var fightRobotArmy = function (robots) {
+    console.log('fight');
+};
+// interface vs type
+// interface is a new name to be used anywhere
+// interface is cleaner syntax
+// type assertion
+var army = {};
+army.magic = 'fireball';
+// Function
+var myFn = function (robots) {
+    console.log('fight on again');
+};
+// Class
+var Animal = /** @class */ (function () {
+    function Animal(sound) {
+        this.sing = 'lalalal';
+        this.sing = sound;
+    }
+    Animal.prototype.greet = function () {
+        return "Hello " + this.sing;
+    };
+    return Animal;
+}());
+var lion = new Animal('RAAAWR');
+console.log(lion.greet());
+// union type |
+var confused = 'abc123';
