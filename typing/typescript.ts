28ea13d8b89f24ba5838a7d4d5533ef2a0146a7a
Tim Urista <timothy.urista@gmail.com>
2018-08-13 18:58:15 -0700

    add more typescript examples

--- typing/typescript.ts
+++ typing/typescript.ts
@@ -3,3 +3,41 @@ const mySum = (a: number, b: number) => {
 }
 
 console.log(mySum(2,4));
+
+let isCool: boolean = true;
+
+let age: number = 56;
+
+let favoriteQuote: string = `I'm not all ${age}`;
+
+// arrays
+let pets: string[] = [ 'cat', 'dog', 'pig'];
+let pets2: Array<string> = ['lion', 'dragon', 'lizard'];
+
+let wizard: object = {
+    a: 'John'
+};
+
+console.log(wizard)
+
+// Tuple
+let basket: [string, number];
+
+basket = ['basketball', 5];
+// handy for certain things
+
+// Enum
+enum Size {
+  small = 1,
+  medium = 2,
+  large = 3
+};
+
+let sizeName: string = Size[2];
+
+// enums let us to define set of named constants
+// directions, or value associated with it
+
+
+
+
