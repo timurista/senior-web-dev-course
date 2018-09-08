6b2afb9b1db0103308df68e72df16a5139ae9f53
Tim Urista <timothy.urista@gmail.com>
2018-09-08 16:54:35 -0700

    update rank to handle edge case and allow localhost to hit it directly

--- aws/rankly/handler.js
+++ aws/rankly/handler.js
@@ -1,13 +1,16 @@
 'use strict';
-const emojis = [
-  '😀', '😁', '🤣', '😃', '😍', '🔶', '🔷', '🚀'
-];
 
 module.exports.rank = async (event, context) => {
   const rank = event.queryStringParameters.rank;
-  const rankEmoji = emojis[rank > emojis.length ? emojis.length - 1 : rank];
+  const emojis = [
+  '😀', '😁', '🤣', '😃', '😍', '🔶', '🔷', '🚀'
+  ];
+  const rankEmoji = emojis[rank >= emojis.length ? emojis.length - 1 : rank];
   return {
     statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
     body: JSON.stringify({
       message: 'Rresponded with emoji',
       input: rankEmoji,
