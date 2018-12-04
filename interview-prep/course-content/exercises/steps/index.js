8033dcedeb71d7942f44cc062c4d34206c92ad52
Tim Urista <timothy.urista@gmail.com>
2018-12-04 06:32:51 -0700

    add recursive solution for steps

--- interview-prep/course-content/exercises/steps/index.js
+++ interview-prep/course-content/exercises/steps/index.js
@@ -23,4 +23,24 @@ function steps(n) {
   }
 }
 
+// if possible try iterative solution then do
+// recurisve solution
+function steps(n, row = 0, stair = '') {
+  if (n === row) {
+    return;
+  }
+
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+
+  if (stair.length <= row) {
+    stair += '#';
+  } else {
+    stair += ' ';
+  }
+  steps(n, row, stair);
+}
+
 module.exports = steps;
