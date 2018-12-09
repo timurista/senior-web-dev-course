98949bb774f88097228a38cc8167e5149ec77e64
Tim Urista <timothy.urista@gmail.com>
2018-12-09 06:29:10 -0800

    add solution for stack class

--- interview-prep/course-content/exercises/stack/index.js
+++ interview-prep/course-content/exercises/stack/index.js
@@ -10,6 +10,22 @@
 //   s.pop(); // returns 2
 //   s.pop(); // returns 1
 
-class Stack {}
+class Stack {
+  constructor() {
+    this.stack = []
+  }
+  // First in Last out
+  push(el) {
+    this.stack.push(el)
+  }
+
+  pop() {
+    return this.stack.pop()
+  }
+
+  peek() {
+    return this.stack[this.stack.length - 1];
+  }
+}
 
 module.exports = Stack;
