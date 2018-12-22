9bbf8fa82c86d8e2bd41f1caa1e6e8f50ee824ea
Tim Urista <timothy.urista@gmail.com>
2018-12-22 07:41:56 -0800

    solve bubble sort

--- interview-prep/course-content/exercises/sorting/index.js
+++ interview-prep/course-content/exercises/sorting/index.js
@@ -2,19 +2,22 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
-
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
 }
 
-function selectionSort(arr) {
+function selectionSort(arr) {}
 
-}
+function mergeSort(arr) {}
 
-function mergeSort(arr) {
-
-}
-
-function merge(left, right) {
-
-}
+function merge(left, right) {}
 
 module.exports = { bubbleSort, selectionSort, mergeSort };
