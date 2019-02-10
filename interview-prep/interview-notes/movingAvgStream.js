442769fcf6dfcd043aa5877511cdda0b30daed2b
Tim Urista <timothy.urista@gmail.com>
2019-02-10 09:50:25 -0800

    add solution to moving avg stream

--- interview-prep/interview-notes/movingAvgStream.js
+++ interview-prep/interview-notes/movingAvgStream.js
@@ -5,45 +5,27 @@ class Node {
   }
 }
 
-/**
- * Initialize your data structure here.
- * @param {number} size
- */
-class MovingAverage {
-  constructor(size = 1) {
-    this.head = null;
+class LinkedList {
+  constructor(node = null) {
+    this.head = node;
+    this.tail = this.head;
     this.length = 0;
-    this.size = size;
   }
 
-  enqueue(item) {
+  append(item) {
     const node = new Node(item);
-    if (!this.head) {
-      this.head = node;
-      this.length++;
-      return;
-    }
-
-    for (let current of this) {
-      if (!current.next) {
-        current.next = node;
-        this.length++;
-        if (this.length > this.size) {
-          this.head = this.head.next;
-          this.length--;
-        }
-        return;
-      }
-    }
+    this.tail.next = node;
+    this.tail = node;
+    this.length++;
+    return this;
   }
 
-  get average() {
-    let sum = 0;
-    console.log("HEAD", this.head);
+  size() {
+    let s = 0;
     for (let n of this) {
-      sum += n.val;
+      l++;
     }
-    return sum / this.length;
+    return s;
   }
 
   *[Symbol.iterator]() {
@@ -56,11 +38,60 @@ class MovingAverage {
 }
 
 /**
+ * Initialize your data structure here.
+ * @param {number} size
+ */
+class MovingAverage {
+  constructor(size = 1) {
+    this.list;
+    this.size = size;
+  }
+
+  add(item) {
+    if (!this.list) {
+      this.list = new LinkedList(new Node(item));
+      return this;
+    }
+
+    this.list.append(item);
+    console.log("LSIT", this.list, this.list.size());
+
+    if (this.list.length > this.size) {
+      this.list.head = this.list.head.next;
+      this.list.length--;
+    }
+
+    // for (let current of this) {
+    //   if (!current.next) {
+    //     current.next = node;
+    //     this.length++;
+    //     if (this.length > this.size) {
+    //       this.head = this.head.next;
+    //       this.length--;
+    //     }
+    //     return;
+    //   }
+    // }
+  }
+
+  get average() {
+    let sum = 0;
+    console.log("HEAD", this.head);
+    console.log("TAIL", this.tail);
+
+    for (let n of this.list) {
+      sum += n.val;
+    }
+    return sum / this.length;
+  }
+}
+
+/**
  * @param {number} val
  * @return {number}
  */
 MovingAverage.prototype.next = function(val) {
-  this.enqueue(val);
+  this.add(val);
 
   return this.average;
 };
@@ -79,10 +110,10 @@ console.log(mvAvg.next(10));
 console.log(mvAvg.next(3));
 console.log(mvAvg.next(5));
 
-const genNum = () => Math.floor(100 * Math.random() + 5);
-const rand = new Array(100000).fill(0).map(c => genNum());
-// console.log(rand.slice(100, 104));
-for (let i of rand) {
-  console.log(mvAvg.next(i));
-}
+// const genNum = () => Math.floor(100 * Math.random() + 5);
+// const rand = new Array(100000).fill(0).map(c => genNum());
+// // console.log(rand.slice(100, 104));
+// for (let i of rand) {
+//   console.log(mvAvg.next(i));
+// }
 // [null,1.0,5.5,4.66667,6.0]
