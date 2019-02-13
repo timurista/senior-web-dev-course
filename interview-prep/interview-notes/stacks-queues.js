491b330498638e16a5b8a0043608cc05270d3cc3
Tim Urista <timothy.urista@gmail.com>
2019-02-13 06:43:38 -0800

    add notes for js underhood notes

---
+++ interview-prep/interview-notes/stacks-queues.js
@@ -0,0 +1,16 @@
+// Stacks
+
+// google
+//udemy
+// youtube
+
+// Arrays or linked lists to build
+// Arrays
+// cached locality faster when accessing in memory
+// got to double memory
+
+// linked lists dynamic memory
+// doesnt need to change
+
+// Queues
+// LinkedLists not Arrays
---
+++ interview-prep/interview-notes/stacks-queues.md
@@ -0,0 +1,14 @@
+## Stacks
+
+LIFO
+last in, first out
+
+## Queues
+
+FIFO
+first in, first out
+wait list to wait
+or wait to check in.
+Uses queues
+Enqueue / dequeue
+peek, eunqueue, dequeue all O (1)
