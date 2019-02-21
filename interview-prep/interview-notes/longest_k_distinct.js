0e2a8cc28e6b723fea613d0a359b68b34d3bc204
Tim Urista <timothy.urista@gmail.com>
2019-02-21 09:10:55 -0800

    add solution for minesweeper

---
+++ interview-prep/interview-notes/longest_k_distinct.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var lengthOfLongestSubstringKDistinct = function(s, k) {
+  // start middle of string
+  // wiggle left
+  // wiggle right
+  // if length
+  let map = {};
+  // for (let i = s) {
+
+  // }
+};
