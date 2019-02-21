c124f3381b6556a3699b230295b094637d4d55b5
Tim Urista <timothy.urista@gmail.com>
2019-02-21 07:30:14 -0800

    add solution for is one away

--- interview-prep/interview-notes/tempCodeRunnerFile.js
+++ interview-prep/interview-notes/tempCodeRunnerFile.js
@@ -54,6 +54,40 @@ function is_one_away(a, b) {
   return true;
 }
 
+function is_one_away_diff_length(A, B) {
+  let countDiff = 0;
+  let i = 0;
+  while (i < A.length) {
+    console.log(countDiff);
+    if (A[i + countDiff] === B[i]) {
+      i++;
+    } else {
+      countDiff++;
+      if (countDiff > 1) return false;
+    }
+  }
+  return true;
+}
+
+var is_one_away = (str1, str2) => {
+  if (Math.abs(str1.length - str2.length) > 2) {
+    return false;
+  }
+  if (str1.length === str2.length) {
+    let countDiff = 0;
+    for (let i = 0; i < str1.length; i++) {
+      if (str1[i] !== str2[i]) countDiff++;
+      if (countDiff > 1) return false;
+    }
+    return true;
+  }
+  if (str1.length > str2.length) {
+    return is_one_away_diff_length(str1, str2);
+  } else {
+    return is_one_away_diff_length(str2, str1);
+  }
+};
+
 console.log(is_one_away("abcde", "abfde"));
 console.log(is_one_away("abcde", "abde"));
 console.log(is_one_away("xyz", "xyaz"));
