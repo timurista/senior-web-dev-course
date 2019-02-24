48f2c39e5d63746b6b7f8b3891c75e59dc55456e
Tim Urista <timothy.urista@gmail.com>
2019-02-24 10:09:08 -0800

    add solution for linked list from end

--- interview-prep/interview-notes/xor-llist.js
+++ interview-prep/interview-notes/xor-llist.js
@@ -26,4 +26,54 @@ root.child.child = new Node(3);
 root.child.child.child = new Node(2);
 root.child.child.child.child = new Node(1);
 
-console.log(root)
+function nth_from_end(head, n) {
+  if (!head) return null;
+
+  let first = head;
+  let second = head;
+
+  while (n > 0) {
+    if (second === null) return null;
+    second = second.child;
+    n--;
+  }
+
+  // now continue iterating to the end
+  while (second !== null) {
+    first = first.child;
+    second = second.child;
+  }
+  return first;
+
+  // let current = head;
+  // let first = head;
+  // // let lastIdx = 0;
+  // let count = 0;
+  // // let found = null;
+  // while (current) {
+  //   current = current.child;
+  //   count++;
+  // }
+  // if (n > count) return null;
+  // let found = head;
+  // let lastIdx = count - n;
+
+  // while (lastIdx > 0) {
+  //   found = found.child;
+  //   lastIdx--;
+  // }
+  // return found;
+}
+
+// console.log(root)
+console.log(nth_from_end(root, 3))
+console.log(nth_from_end(root, 6))
+
+// console.log(root)
+let head2 = new Node(1);
+head2.child = new Node(2);
+head2.child.child = new Node(3);
+
+console.log(nth_from_end(head2, 3))
+console.log(nth_from_end(head2, 1))
+console.log(nth_from_end(null, 1))
